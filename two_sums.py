a4449ed3146cfb7ce541d094db5e5cf2ffca7b18
robsen123-fexil <robsenteshoma1@gmail.com>
2023-10-21 18:15:19 +0300

    easy

---
+++ two_sums.py
@@ -0,0 +1,9 @@
+class solution:
+    def sol(self , nums, target):
+        
+        for i in range(len(nums)):
+         for j in range(i + 1, len(nums)):
+            if target ==nums[i]+nums[j]:
+               return [i,j]
+                         
+       
