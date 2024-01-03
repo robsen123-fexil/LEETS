20a941958fc5932a39f850238def28930f5523f2
robsen123-fexil <robsenteshoma1@gmail.com>
2024-01-03 13:37:37 +0300

    hghv

--- buy&sell_stock.py
+++ buy&sell_stock.py
@@ -1,14 +1,32 @@
-class solution:
-    def stock(self , price:list)->int:
-        idn=price.index(min(price))
-        res=[]
-        print(idn)
-        for i in range(idn,len(price)):
+class Solution:
+    def stock(self, price: list) -> int:
+        idn = price.index(min(price))
+        res = []
+
+        for i in range(idn, len(price)):
             res.append(price[i])
-        print(res)
-        
-        lr=res.index(max(res))   
-        return lr
-obj=solution()
-result=obj.stock([9,3,89,7,10,6])    
-print(result)
+
+        lr = res.index(max(res))
+        lr += 1
+        st = max(res)
+        nn = None
+
+        if len(price) >= 2 and price[0] == price[1]:
+            return 0
+
+        for i in range(len(price)):
+            if len(res) == 1:
+                nn = 0
+                break
+            if price[i] == st:
+                nn = price.index(price[i])
+                return nn + 1
+
+        if nn is None:
+            return 0
+        return nn + 1
+
+
+obj = Solution()
+result = obj.stock([1, 2])
+print(result)  
