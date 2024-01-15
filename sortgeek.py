c501871b9a1a791ae8bd9986cee0c39e3023a8c2
robsen123-fexil <robsen123-fexil>
2024-01-15 12:58:17 +0300

    geeks

--- sortgeek.py
+++ sortgeek.py
@@ -1,9 +1,9 @@
 class Solution:
-    def sort(self, a: list, n: int) -> list:
+    def sort(self, a: list) -> list:
         sor = []
         sor=sorted(a)
         return sor
 obj = Solution()
 ar = [32, 322, 22]
-result = obj.sort(ar, 3)
+result = obj.sort(ar)
 print(result)
