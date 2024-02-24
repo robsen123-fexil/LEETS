33d39fdf41c81258fde2fab75dab8d586642c8ef
robsen123-fexil <robsenteshoma1@gmail.com>
2024-02-24 23:35:12 +0300

    geeks

--- sortgeek.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def sort(self, a: list) -> list:
-        sor = []
-        sor=sorted(a)
-        return sor
-obj = Solution()
-ar = [32, 322, 22]
-result = obj.sort(ar)
-print(result)
